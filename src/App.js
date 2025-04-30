f408fd06037abca7a29712c520ffc286384077d5
Vansh Patel <patel.vansh1607@gmail.com>
2025-04-30 19:38:35 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import RecipeForm from './components/RecipeForm';
+import RecipeList from './components/RecipeList';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    const stored = JSON.parse(localStorage.getItem('recipes'));
+    if (stored) setRecipes(stored);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('recipes', JSON.stringify(recipes));
+  }, [recipes]);
+
+  const addRecipe = (recipe) => {
+    setRecipes([recipe, ...recipes]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>🍳 Recipe Book</h1>
+      <RecipeForm onAdd={addRecipe} />
+      <RecipeList recipes={recipes} />
     </div>
   );
-}
+};
 
 export default App;
